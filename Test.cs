5c87aa057ec0cc11ffc4cc4ce845763b3317a07b
nicholassyk <100067351@studens.swinburne.edu.my>
2017-05-26 07:18:58 +0800

    Initialize

---
+++ Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SwinGameSDK;
+using NUnit.Framework;
+
+namespace MyGame
+{
+	[TestFixture ()]
+	public class Test
+	{
+		//Test mouse and ship misalignment.
+		[Test ()]
+		public void TestDeployment ()
+		{
+			int Row = 0;
+			int Col = 0;
+			Point2D mouse = SwinGame.MousePosition ();
+
+			Row = Convert.ToInt32 (Math.Floor ((mouse.Y - UtilityFunctions.FIELD_TOP) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
+			Col = Convert.ToInt32 (Math.Floor ((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
+
+			Assert.IsTrue (-3 == Row);
+			Assert.IsTrue (-9 == Col);
+		}
+	}
+}
---
+++ src/DiscoveryController.cs
@@ -0,0 +1,99 @@
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using SwinGameSDK;
+
+/// <summary>
+/// The battle phase is handled by the DiscoveryController.
+/// </summary>
+static class DiscoveryController
+{
+	public static int change = 1;
+	/// <summary>
+	/// Handles input during the discovery phase of the game.
+	/// </summary>
+	/// <remarks>
+	/// Escape opens the game menu. Clicking the mouse will
+	/// attack a location.
+	/// </remarks>
+	public static void HandleDiscoveryInput()
+	{
+		/// <summary>
+		/// Changes the background sound
+		/// </summary>
+		if (SwinGame.KeyTyped (KeyCode.vk_a)) {
+			SwinGame.StopMusic();
+
+			if (change == 1) {
+				SwinGame.PlayMusic (GameResources.GameMusic ("B1"));
+				change++;
+			} else if (change == 2) {
+				SwinGame.PlayMusic (GameResources.GameMusic ("B2"));
+				change++;
+			} else {
+				SwinGame.PlayMusic (GameResources.GameMusic ("Background"));
+				change = 1;
+			}
+		}
+
+		if (SwinGame.KeyTyped(KeyCode.vk_ESCAPE)) {
+			GameController.AddNewState(GameState.ViewingGameMenu);
+		}
+
+		if (SwinGame.MouseClicked(MouseButton.LeftButton)) {
+			DoAttack();
+		}
+	}
+
+	/// <summary>
+	/// Attack the location that the mouse if over.
+	/// </summary>
+	private static void DoAttack()
+	{
+		Point2D mouse = default(Point2D);
+
+		mouse = SwinGame.MousePosition();
+
+		//Calculate the row/col clicked
+		int row = 0;
+		int col = 0;
+		row = Convert.ToInt32(Math.Floor((mouse.Y - UtilityFunctions.FIELD_TOP) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
+		col = Convert.ToInt32(Math.Floor((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
+
+		if (row >= 0 & row < GameController.HumanPlayer.EnemyGrid.Height) {
+			if (col >= 0 & col < GameController.HumanPlayer.EnemyGrid.Width) {
+				GameController.Attack(row, col);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Draws the game during the attack phase.
+	/// </summary>s
+	public static void DrawDiscovery()
+	{
+		const int SCORES_LEFT = 172;
+		const int SHOTS_TOP = 157;
+		const int HITS_TOP = 206;
+		const int SPLASH_TOP = 256;
+
+		if ((SwinGame.KeyDown(KeyCode.vk_LSHIFT) | SwinGame.KeyDown(KeyCode.vk_RSHIFT)) & SwinGame.KeyDown(KeyCode.vk_c)) {
+			UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, true);
+		} else {
+			UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, false);
+		}
+
+			UtilityFunctions.DrawSmallField(GameController.HumanPlayer.PlayerGrid, GameController.HumanPlayer);
+			UtilityFunctions.DrawMessage();
+
+		SwinGame.DrawText(GameController.HumanPlayer.Shots.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SHOTS_TOP);
+		SwinGame.DrawText(GameController.HumanPlayer.Hits.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, HITS_TOP);
+		SwinGame.DrawText(GameController.HumanPlayer.Missed.ToString(), Color.White, GameResources.GameFont("Menu"), SCORES_LEFT, SPLASH_TOP);
+		//Adds the instruction for the background sound
+		SwinGame.DrawTextLines ("Press A to change the sound", Color.White, Color.Transparent, GameResources.GameFont ("Menu"), FontAlignment.AlignCenter, (SwinGame.ScreenWidth () / 2) - 50, 570, 400, 15);
+
+	}
+
+}
