b371ad3c3504db0fc89f6b4fe105cc08dd516a61
khaled2095 <100082095@students.swinburne.edu.my>
2017-05-26 16:25:50 +0800

    Khaleds unit test

---
+++ TestState.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+namespace MyGame
+{
+	[TestFixture()]
+	public class TestState
+	{
+		/// <summary>
+		/// This test makes sure if the game state can be added successfully.
+		/// </summary>
+		[Test ()]
+		public void TestStateAdd ()
+		{
+			GameController.AddNewState (GameState.AlteringSettings);
+			Assert.AreEqual (GameController.CurrentState, GameState.AlteringSettings);
+
+			GameController.AddNewState (GameState.EndingGame);
+			Assert.AreEqual (GameController.CurrentState, GameState.EndingGame);
+
+			GameController.EndCurrentState ();
+			Assert.AreEqual (GameController.CurrentState, GameState.AlteringSettings);
+		}
+	}
+}
