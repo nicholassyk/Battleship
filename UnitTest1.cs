79a747deadb1b35de29fa4fd4f034acdc354d02d
Madd9658 <100075105@students.swinburne.edu.my>
2017-05-26 15:41:43 +0800

    -UnitTest1

---
+++ UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+namespace MyGame
+{
+	
+	/// <summary>
+	/// Madelin
+	/// Pass Task 10. 2 part 2
+	/// Unit testing for the gamestate if can be switch 
+	/// </summary>
+	[TestFixture ()]
+	public class UnitTest1
+	{
+		[Test ()]
+		public void TestingSwitchState ()
+		{
+			GameController.SwitchState (GameState.Deploying);
+			Assert.AreEqual (GameController.CurrentState, GameState.Deploying);
+		}
+	}
+}
